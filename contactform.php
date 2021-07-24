16006dd720457945b716e4d60688af4188d3b777
chytran <chytran@users.noreply.github.com>
2021-07-24 12:35:16 -0700

    Setting up form

--- contactform.php
+++ contactform.php
@@ -1,5 +1,15 @@
 <?php
 
 if (isset($_POST['submit'])) {
-    
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "kevintran@1142@gmail.com";
+    $header = "From: " . $mailFrom;
+    $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
+
+    mail($mailTo, $subject, $txt, $header);
+    header("Location: index.php?mailsend");
 }
