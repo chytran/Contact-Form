b0c9a476f6d7fad8cde0677118b9244394766d36
chytran <chytran@users.noreply.github.com>
2021-07-24 12:17:04 -0700

    html set up

--- index.php
+++ index.php
@@ -5,8 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    
+    <main class="main">
+        <p>Send E-Mail</p>
+        <form action="contactform.php" method="post" class="contact-form">
+            <input type="text" name="name" placeholder="Full name">
+            <input type="text" name="mail" placeholder="Your Email">
+            <input type="text" name="subject" placeholder="Subject">
+            <textarea name="message" placeholder="Message"></textarea>
+            <button type="submit" name="submit">Send Mail</button>
+        </form>
+    </main>
 </body>
 </html>
